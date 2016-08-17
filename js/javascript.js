a4f2e2eb40ea2efbd621e3bdc346c049c00e6a75
doritobandito <michael.white308@gmail.com>
2016-08-17 13:38:33 +0800

    Added scrolling active classes

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,24 @@
+function addRemoveActive(anchor) {
+    $(".nav").find(".active").removeClass("active");
+    $(anchor).parent().addClass("active");
+}
+
 $(document).ready(function() {
 
     $('#fullpage').fullpage({
-        anchors:['home', 'con-one', 'con-two', 'con-three']
+        anchors:['home', 'con-one', 'con-two', 'con-three'],
+        menu: '#menu'
     });
 
     var quotes = $(".revolving-text");
     var quoteIndex = -1;
 
-    $(".navbar-header a").on("click", function(){
+    $(".navbar-header a").on("click", function() {
         $(".nav").find(".active").removeClass("active");
     })
 
-    $(".nav a").on("click", function(){
-        $(".nav").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
+    $(".nav a").on("click", function() {
+        addRemoveActive(this);
     });
 
     /*function showNextQuote() {
